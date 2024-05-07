d18ee42e1c9392114f3891de347a43e10c897fd4
Lijiya <Lijiya@DESKTOP-B1S4JGN>
2024-05-07 23:28:04 +0530

    commit

---
+++ Mavenprojectjenkins/src/main/java/jenkinpackage/Sample.java
@@ -0,0 +1,11 @@
+package jenkinpackage;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=30;
+		System.out.println("value of a is "+a);
+	}
+
+}
